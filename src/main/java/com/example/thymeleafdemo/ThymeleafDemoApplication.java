5141fbc587aba757b9c6786ce5cffc9989ecc934
Arina Lazarenko <73884700+AlbinaArina@users.noreply.github.com>
2023-01-11 15:07:34 +0100

    + design to wordlist

--- src/main/java/com/example/thymeleafdemo/ThymeleafDemoApplication.java
+++ src/main/java/com/example/thymeleafdemo/ThymeleafDemoApplication.java
@@ -1,13 +1,41 @@
 package com.example.thymeleafdemo;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.xhtmlrenderer.layout.SharedContext;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @SpringBootApplication
 public class ThymeleafDemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ThymeleafDemoApplication.class, args);
+		generatePDF();
+	}
+
+	public static void generatePDF() throws IOException {
+		File htmlFile = new File("D:\\ПРОЄКТИ\\JAVA\\thymeleaf-demo\\src\\main\\resources\\templates\\words.html");
+
+		Document document = Jsoup.parse(htmlFile, "UTF-8");
+		document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+
+		try (OutputStream outputStream = new FileOutputStream("D:\\ПРОЄКТИ\\JAVA\\thymeleaf-demo\\src\\main\\resources\\templates\\output_1.pdf")) {
+			ITextRenderer renderer = new ITextRenderer();
+			SharedContext sharedContext = renderer.getSharedContext();
+			sharedContext.setPrint(true);
+			sharedContext.setInteractive(false);
+			renderer.setDocumentFromString(document.html());
+			renderer.layout();
+			renderer.createPDF(outputStream);
+		}
+		System.out.println("done");
 	}
 
 }
--- src/main/java/com/example/thymeleafdemo/WordController.java
+++ src/main/java/com/example/thymeleafdemo/WordController.java
@@ -16,15 +16,16 @@ public class WordController {
         words = new ArrayList<>();
         words.add(new Word("apple", "яблуко", "https://www.kindpng.com/picc/m/372-3727789_caramel-apple-clip-art-red-apple-images-clip.png"));
         words.add(new Word("banana", "банан", "https://www.kindpng.com/picc/m/128-1288078_banana-education-fruit-school-peeled-banana-cartoon-png.png"));
-        words.add(new Word("onion", "цибуля", "https://www.kindpng.com/picc/m/5-52172_onion-clipart-onion-slice-onion-clipart-hd-png.png"));words.add(new Word("cucumber", "огірок"));
-        words.add(new Word("melon", "диня"));
-        words.add(new Word("watermelon", "кавун"));
-        words.add(new Word("pineapple", "ананас"));
-        words.add(new Word("tomato", "помідор"));
-        words.add(new Word("potato", "картопля"));
-        words.add(new Word("corn", "кукурудза"));
-        words.add(new Word("pepper", "перець"));
-        words.add(new Word("grapes", "виноград"));
+        words.add(new Word("onion", "цибуля", "https://www.kindpng.com/picc/m/5-52172_onion-clipart-onion-slice-onion-clipart-hd-png.png"));
+        words.add(new Word("cucumber", "огірок", "https://www.kindpng.com/picc/m/51-516375_ingredient-image-cucumber-for-glowing-skin-hd-png.png"));
+        words.add(new Word("melon", "диня", "https://www.kindpng.com/picc/m/153-1534992_cantaloupe-png-clipart-cantaloupe-clipart-png-transparent-png.png"));
+        words.add(new Word("watermelon", "кавун", "https://www.kindpng.com/picc/m/52-527308_watermelon-clipart-clipartlook-watermelon-clip-art-hd-png.png"));
+        words.add(new Word("pineapple", "ананас", "https://www.kindpng.com/picc/m/3-35402_love-pineapple-pizza-meme-hd-png-download.png"));
+        words.add(new Word("tomato", "помідор", "https://www.kindpng.com/picc/m/138-1380513_tomato-png-tomato-clipart-transparent-png.png"));
+        words.add(new Word("potato", "картопля", "https://www.kindpng.com/picc/m/68-681976_-yukon-gold-potato-hd-png-download.png"));
+        words.add(new Word("corn", "кукурудза", "https://www.kindpng.com/picc/m/111-1110281_corn-png-clipart-transparent-corn-clipart-png-download.png"));
+        words.add(new Word("pepper", "перець", "https://www.kindpng.com/picc/m/101-1019240_hot-chili-pepper-tree-clipart-red-chili-pepper.png"));
+        words.add(new Word("grapes", "виноград", "https://www.kindpng.com/picc/m/113-1138853_grapes-fruit-with-name-hd-png-download.png"));
     }
 
 
